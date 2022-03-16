edd7d9880617f6d9e0daa7e13cc7f9522d7aec7c
Scott Humphrey <scott.humphrey@whizzeducation.com>
2022-03-16 09:04:11 +0000

    Added graphics to Sprite

--- src/flash/display/Sprite.ts
+++ src/flash/display/Sprite.ts
@@ -1,7 +1,18 @@
 import { Sprite as PixiSprite } from '@pixi/sprite';
+import { Graphics } from '@pixi/graphics';
 
 class Sprite extends PixiSprite {
+  _graphics: Graphics;
 
+  constructor() {
+    super();
+    this._graphics = new Graphics();
+    this.addChild(this._graphics);
+  }
+
+  get graphics():Graphics {
+    return this._graphics;
+  }
 }
 
 export { Sprite };
